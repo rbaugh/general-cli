d0fa9193fb57542754030e881d4b0e32a9b956ce
Blake Cerecero <Blake@DigitalBlake.com>
2020-06-07 22:23:10 -0500

    [zsh] Updated theme unit test data URL in scripts
    Updated the URL in the wp-theme-unit-data function to call in the newer Gutenberg unit theme test data.

--- custom-scripts/global-scripts.zsh
+++ custom-scripts/global-scripts.zsh
@@ -14,7 +14,7 @@ wp-theme-unit-data() {
 	WORKING_DIR=$(pwd);
 
 	cd $SERVER_DIR
-	curl https://wpcom-themes.svn.automattic.com/demo/theme-unit-test-data.xml >> theme-unit-test-data.xml
+	curl https://raw.githubusercontent.com/WPTT/theme-unit-test/master/themeunittestdata.wordpress.xml >> theme-unit-test-data.xml
 	wp plugin install wordpress-importer --activate --allow-root
 	wp import theme-unit-test-data.xml --authors=create --allow-root
 	rm theme-unit-test-data.xml
